112b8129f874c4d09c316e6e041c5cf8cb21897e
aelassas <akram.elassas@gmail.com>
2025-08-11 01:25:59 +0100

    fix(cli): make first argument case-insensitive by normalizing input

--- src/Servy.CLI/Helpers/Helper.cs
+++ src/Servy.CLI/Helpers/Helper.cs
@@ -35,7 +35,7 @@ namespace Servy.CLI.Helpers
             var verbs = Assembly.GetExecutingAssembly()
                  .GetTypes()
                  .Where(t => t.GetCustomAttribute<VerbAttribute>() != null)
-                 .Select(t => t.GetCustomAttribute<VerbAttribute>().Name)
+                 .Select(t => t.GetCustomAttribute<VerbAttribute>().Name.ToLowerInvariant())
                  .ToArray();
             return verbs;
         }
