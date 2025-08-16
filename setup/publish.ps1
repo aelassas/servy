28de283c587eb8f6b084873ae5375c17974695d1
aelassas <akram.elassas@gmail.com>
2025-08-16 20:55:15 +0100

    feat(setup): update version in publish.ps1

--- setup/publish.ps1
+++ setup/publish.ps1
@@ -11,7 +11,7 @@ $ErrorActionPreference = "Stop"
 $startTime = Get-Date
 
 # === CONFIGURATION ===
-$version      = "1.0.0"
+$version      = "1.0"
 $AppName      = "servy"
 $BuildConfig  = "Release"
 $Platform     = "x64"
