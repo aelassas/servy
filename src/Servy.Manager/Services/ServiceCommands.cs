57d53ae67df0aac4e7ff0a736f4efd9a41767e9d
aelassas <akram.elassas@gmail.com>
2025-08-21 02:33:14 +0100

    fix(manager): handle background search task cancellation gracefully

--- src/Servy.Manager/Services/ServiceCommands.cs
+++ src/Servy.Manager/Services/ServiceCommands.cs
@@ -15,6 +15,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -70,18 +71,17 @@ namespace Servy.Manager.Services
         }
 
         /// <inheritdoc />
-        public async Task<List<Service>> SearchServicesAsync(string searchText)
+        public async Task<List<Service>> SearchServicesAsync(string searchText, CancellationToken cancellationToken = default)
         {
             //await DummyHelper.InsertDummyServices("Data Source=Servy.db", 5);
 
-            var results = await _serviceRepository.SearchDomainServicesAsync(_serviceManager, searchText ?? string.Empty);
+            var results = await _serviceRepository.SearchDomainServicesAsync(_serviceManager, searchText ?? string.Empty, cancellationToken);
 
             // Materialize all Service models in memory immediately
             var services = await Task.Run(() => results.Select(ServiceMapper.ToModel).ToList());
 
             return services;
         }
-
 
         /// <inheritdoc />
         public async Task<bool> StartServiceAsync(Service service)
