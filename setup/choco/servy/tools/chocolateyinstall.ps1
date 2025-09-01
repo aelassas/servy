3b888b31c7b050d02e7290299e48a533883a945f
aelassas <akram.elassas@gmail.com>
2025-09-01 16:15:32 +0100

    feat(setup): update winget and choco manifests

--- setup/choco/servy/tools/chocolateyinstall.ps1
+++ setup/choco/servy/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName    = 'servy'
 $installerType  = 'exe'
 $url64          = 'https://github.com/aelassas/servy/releases/download/v1.0/servy-1.0-net48-x64-installer.exe'
-$checksum64     = 'CE60EAC89E1ACAE4BC9E7DBFBBEBB9EA78CB2DF8BC76D1039DBD2B8081C2AE08'
+$checksum64     = '2AC9B5232ECF33CC3E5279747CE3AF970F092C43145E78AF90DF2E718E728A5C'
 $checksumType64 = 'sha256'
 $silentArgs     = '/VERYSILENT /NORESTART /SUPPRESSMSGBOXES'
 
