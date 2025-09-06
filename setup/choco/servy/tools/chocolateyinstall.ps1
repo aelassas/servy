45d8d7c4a91500360eaa4576883055a9d676f3ab
aelassas <akram.elassas@gmail.com>
2025-09-06 09:02:10 +0100

    fix(installer): app icons not showing

--- setup/choco/servy/tools/chocolateyinstall.ps1
+++ setup/choco/servy/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageName    = 'servy'
 $toolsDir       = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $installerType  = 'exe'
 $url64          = 'https://github.com/aelassas/servy/releases/download/v1.0/servy-1.0-net8.0-x64-installer.exe'
-$checksum64     = 'EE65DC56EF0B3D9D1E1081116592AB2FC8DFDC084BFFEBE744E6526B77DF58F3'
+$checksum64     = '3EA6AB7CC3DACD4FBCD1D2FAF0421F90340ACE88DA7C7D7BD42A6B43A670364C'
 $checksumType64 = 'sha256'
 $silentArgs     = '/VERYSILENT /NORESTART /SUPPRESSMSGBOXES'
 
