813bd62c4797824beed8250cc99c44dd65646fa4
aelassas <akram.elassas@gmail.com>
2025-09-06 07:43:30 +0100

    fix(installer): inno setup

--- setup/choco/servy/tools/chocolateyinstall.ps1
+++ setup/choco/servy/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageName    = 'servy'
 $toolsDir       = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $installerType  = 'exe'
 $url64          = 'https://github.com/aelassas/servy/releases/download/v1.0/servy-1.0-net8.0-x64-installer.exe'
-$checksum64     = 'C4BF252FFB3DE5B2A993C044416021AA0C5243C8F704556F19CA68BD3AAA74A2'
+$checksum64     = 'CDFE7BF3DB945CE5F1B262E291C40F53C1D76E4228E0166336B229D49D5ABDEE'
 $checksumType64 = 'sha256'
 $silentArgs     = '/VERYSILENT /NORESTART /SUPPRESSMSGBOXES'
 
