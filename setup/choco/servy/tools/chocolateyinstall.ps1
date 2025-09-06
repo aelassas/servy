100d3e06590e0fbf9aca7618c7b7a1b7245d674b
aelassas <akram.elassas@gmail.com>
2025-09-06 07:10:21 +0100

    fix(installer): inno setup icons

--- setup/choco/servy/tools/chocolateyinstall.ps1
+++ setup/choco/servy/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName    = 'servy'
 $installerType  = 'exe'
 $url64          = 'https://github.com/aelassas/servy/releases/download/v1.0/servy-1.0-net48-x64-installer.exe'
-$checksum64     = '31AC48876DA5994D2D708F5C22715247B8814C9C162F7B2E232CF2B6ED6E3C01'
+$checksum64     = 'F510EAE335BFCD0D68BB81F3B5C0EDD17703EC4C7C55E36183EF86CF5464E002'
 $checksumType64 = 'sha256'
 $silentArgs     = '/VERYSILENT /NORESTART /SUPPRESSMSGBOXES'
 
