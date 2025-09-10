18ffb073b16025ab4abe1be12eaa5f351687f06e
aelassas <akram.elassas@gmail.com>
2025-09-10 22:47:38 +0100

    chore(ci): add WinGet and Chocolatey package automation workflows

--- setup/choco/servy/tools/chocolateyinstall.ps1
+++ setup/choco/servy/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName    = 'servy'
 $toolsDir       = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $installerType  = 'exe'
-$url64          = 'https://github.com/aelassas/servy/releases/download/v1.1/servy-1.1-net8.0-x64-installer.exe'
+$url64          = 'https://github.com/aelassas/servy/releases/download/v1.1/servy-1.0-net8.0-x64-installer.exe'
 $checksum64     = '8D8EB49758E388EABD60A09B4CE609727F5DADB111532D925183D1CA4C8633A4'
 $checksumType64 = 'sha256'
 $silentArgs     = '/VERYSILENT /NORESTART /SUPPRESSMSGBOXES'
