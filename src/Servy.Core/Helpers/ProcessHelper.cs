1bd0af46f38b5f8ba7e810d902204d9d2b422531
aelassas <akram.elassas@gmail.com>
2025-09-28 20:42:02 +0100

    fix(tests): ProcessHelper.cs

--- src/Servy.Core/Helpers/ProcessHelper.cs
+++ src/Servy.Core/Helpers/ProcessHelper.cs
@@ -31,6 +31,7 @@ namespace Servy.Core.Helpers
         /// </summary>
         /// <param name="pid">The process ID.</param>
         /// <returns>The CPU usage percentage rounded to one decimal place, or 0 if unavailable.</returns>
+        [ExcludeFromCodeCoverage]
         public static double GetCPUUsage(int pid)
         {
             try
