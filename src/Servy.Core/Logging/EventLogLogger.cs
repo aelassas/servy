64c10e55b8a55dadce60cb67467ef682e98835eb
aelassas <akram.elassas@gmail.com>
2025-10-19 11:29:05 +0100

    feat(logging): add Event ID to Info, Warning, and Error service logs

--- src/Servy.Core/Logging/EventLogLogger.cs
+++ src/Servy.Core/Logging/EventLogLogger.cs
@@ -5,7 +5,7 @@ using System.Diagnostics.CodeAnalysis;
 namespace Servy.Core.Logging
 {
     /// <summary>
-    /// Logs messages to the Windows Event Log.
+    /// Logs messages to the Windows Event Log with Event IDs.
     /// </summary>
     [ExcludeFromCodeCoverage]
     public class EventLogLogger : ILogger
@@ -14,6 +14,11 @@ namespace Servy.Core.Logging
 
         ///<inheritdoc/>
         public string Prefix { get; set; }
+
+        // Default Event IDs per level
+        private const int InfoEventId = 1000;
+        private const int WarningEventId = 2000;
+        private const int ErrorEventId = 3000;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="EventLogLogger"/> class with the specified source.
@@ -35,20 +40,20 @@ namespace Servy.Core.Logging
         /// <inheritdoc/>
         public void Info(string message)
         {
-            _eventLog.WriteEntry(Format(message), EventLogEntryType.Information);
+            _eventLog.WriteEntry(Format(message), EventLogEntryType.Information, InfoEventId);
         }
 
         /// <inheritdoc/>
         public void Warning(string message)
         {
-            _eventLog.WriteEntry(Format(message), EventLogEntryType.Warning);
+            _eventLog.WriteEntry(Format(message), EventLogEntryType.Warning, WarningEventId);
         }
 
         /// <inheritdoc/>
         public void Error(string message, Exception ex = null)
         {
             var fullMessage = ex != null ? $"{message}\n{ex}" : message;
-            _eventLog.WriteEntry(Format(fullMessage), EventLogEntryType.Error);
+            _eventLog.WriteEntry(Format(fullMessage), EventLogEntryType.Error, ErrorEventId);
         }
 
         #region Private Helpers
