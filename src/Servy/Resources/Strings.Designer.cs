2cf5e81d3a229d72cc68d5436d4efece77c2ad4b
aelassas <akram.elassas@gmail.com>
2025-11-19 10:28:51 +0100

    chore: update service display name info text

--- src/Servy/Resources/Strings.Designer.cs
+++ src/Servy/Resources/Strings.Designer.cs
@@ -251,7 +251,7 @@ namespace Servy.Resources {
         }
         
         /// <summary>
-        ///   Recherche une chaîne localisée semblable à Optional. The human-readable name shown in the Windows Services console (services.msc). If left empty, the service name will be used instead..
+        ///   Recherche une chaîne localisée semblable à The human-readable name shown in the Windows Services console (services.msc). If left empty, the service name will be used instead..
         /// </summary>
         public static string Info_ServiceDisplayName {
             get {
