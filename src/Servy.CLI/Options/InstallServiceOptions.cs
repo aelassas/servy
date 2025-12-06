40308c8b70010b405e4164f9d15563a24f2c06e6
aelassas <akram.elassas@gmail.com>
2025-12-06 14:09:30 +0100

    chore: update user permissions info text

--- src/Servy.CLI/Options/InstallServiceOptions.cs
+++ src/Servy.CLI/Options/InstallServiceOptions.cs
@@ -183,7 +183,7 @@ namespace Servy.CLI.Options
         /// Gets or sets the Windows service account username.
         /// Optional.
         /// </summary>
-        [Option("user", HelpText = "The service account username (e.g., .\\username, DOMAIN\\username, or DOMAIN\\gMSA$ for Group Managed Service Accounts). Make sure to grant full access on %ProgramData%\\Servy to NT AUTHORITY\\NetworkService, NT AUTHORITY\\LocalService, or whichever account runs the service.")]
+        [Option("user", HelpText = "The service account username (e.g., .\\username, DOMAIN\\username, or DOMAIN\\gMSA$ for Group Managed Service Accounts). Make sure to grant full access on %ProgramData%\\Servy to whichever account runs the service.")]
         public string User { get; set; }
 
         /// <summary>
