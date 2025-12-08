86a770390105bd3ec4f0116ed0650a6d1cf226a5
aelassas <akram.elassas@gmail.com>
2025-12-08 05:28:53 +0100

    chore: update max rotations validation message

--- src/Servy.Manager/Resources/Strings.Designer.cs
+++ src/Servy.Manager/Resources/Strings.Designer.cs
@@ -673,7 +673,7 @@ namespace Servy.Manager.Resources {
         }
         
         /// <summary>
-        ///   Recherche une chaîne localisée semblable à Maximum rotations must be a number greater than or equal to 0..
+        ///   Recherche une chaîne localisée semblable à Max rotations must be a number greater than or equal to 0..
         /// </summary>
         public static string Msg_InvalidMaxRotations {
             get {
