ceb97b3fa4ce66b07e1466d7fb39b875499854d8
aelassas <akram.elassas@gmail.com>
2026-01-30 22:12:44 +0100

    fix(tests): ServiceDomainTests.cs

--- tests/Servy.Core.UnitTests/Domain/ServiceDomainTests.cs
+++ tests/Servy.Core.UnitTests/Domain/ServiceDomainTests.cs
@@ -142,6 +142,9 @@ namespace Servy.Core.UnitTests.Domain
                 FailureProgramStartupDirectory = "C:\\failureProgramDir",
                 FailureProgramParameters = "--failureProgramArg",
 
+                ActiveStdoutPath = "C:\\stdout.log",
+                ActiveStderrPath = "C:\\stderr.log",
+
                 PreStopExecutablePath = "C:\\pre-stop.exe",
                 PreStopStartupDirectory = "C:\\preStopDir",
                 PreStopParameters = "--preStopArg",
