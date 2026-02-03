514aaf2d7f765a94cc826d4c359ccc469cc690b6
aelassas <akram.elassas@gmail.com>
2026-02-03 10:25:15 +0100

    refactor(manager): DependencyService.cs

--- src/Servy.Manager/Models/DependencyService.cs
+++ src/Servy.Manager/Models/DependencyService.cs
@@ -1,16 +1,9 @@
-﻿using Servy.Core.Services;
-
-namespace Servy.Manager.Models
+﻿namespace Servy.Manager.Models
 {
     /// <summary>
     /// Represents a Windows service being tracked for console tailing.
     /// </summary>
     public class DependencyService : ServiceItemBase
     {
-        /// <summary>
-        /// Service dependency tree.
-        /// </summary>
-        public ServiceDependencyNode Dependencies { get; set; }
-
     }
 }
