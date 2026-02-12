81f3e0df768fc96bd41003e27dbda315664eefbe
Akram El Assas <akram.elassas@gmail.com>
2026-02-12 11:29:12 +0100

    refactor(core): update SecurePassword.cs

--- src/Servy.Core/Helpers/SecurePassword.cs
+++ src/Servy.Core/Helpers/SecurePassword.cs
@@ -201,14 +201,7 @@ namespace Servy.Core.Helpers
                     using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
                     using (var sr = new StreamReader(cs, Encoding.UTF8))
                     {
-                        var sb = new StringBuilder();
-                        char[] buffer = new char[BufferSize];
-                        int read;
-                        while ((read = sr.Read(buffer, 0, buffer.Length)) > 0)
-                        {
-                            sb.Append(buffer, 0, read);
-                        }
-                        return sb.ToString();
+                        return sr.ReadToEnd();
                     }
                 }
                 finally
