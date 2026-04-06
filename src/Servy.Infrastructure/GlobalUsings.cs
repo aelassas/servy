a408eeec230f05ecbf59b44a18de2008dc6f76a2
aelassas <akram.elassas@gmail.com>
2026-04-06 08:10:48 +0100

    fix(core,infra): inconsistent ConfigureAwait(false) usage across async methods (#113)

---
+++ src/Servy.Infrastructure/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿// GlobalUsings.cs
+using Fody;
+
+// This attribute tells ConfigureAwait.Fody to apply .ConfigureAwait(false) 
+// to every await statement in this assembly automatically.
+[assembly: ConfigureAwait(false)]
