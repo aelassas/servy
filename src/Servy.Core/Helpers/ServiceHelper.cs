05ef0184468595e8f1f267b45fafcb168bcb2781
aelassas <akram.elassas@gmail.com>
2026-04-06 05:27:52 +0100

    fix(core): ServiceHelper.GetRunningServyServices: verbose list union can be a single LINQ expression

--- src/Servy.Core/Helpers/ServiceHelper.cs
+++ src/Servy.Core/Helpers/ServiceHelper.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
 using System.ServiceProcess;
 using System.Threading.Tasks;
 
@@ -55,15 +56,7 @@ namespace Servy.Core.Helpers
         /// Gets the names of all currently running Servy services (GUI and CLI).
         /// </summary>
         /// <returns>A list of service names.</returns>
-        public List<string> GetRunningServyServices()
-        {
-            var guiServices = GetRunningServyUIServices();
-            var cliServices = GetRunningServyCLIServices();
-            var services = new List<string>();
-            services.AddRange(guiServices);
-            services.AddRange(cliServices);
-            return services;
-        }
+        public List<string> GetRunningServyServices() => GetRunningServyUIServices().Concat(GetRunningServyCLIServices()).ToList();
 
         /// <summary>
         /// Starts the specified services if they are not already running or pending start,
