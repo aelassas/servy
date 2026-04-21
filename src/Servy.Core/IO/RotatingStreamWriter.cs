42e125f1055df54cf589fdccf1d84e649bd5b852
aelassas <akram.elassas@gmail.com>
2026-04-22 00:59:56 +0100

    fix(infra): DapperExecutor.cs - SpinWait.SpinUntil(() => false, delay) misuses SpinWait as Thread.Sleep

--- src/Servy.Core/IO/RotatingStreamWriter.cs
+++ src/Servy.Core/IO/RotatingStreamWriter.cs
@@ -465,8 +465,8 @@ namespace Servy.Core.IO
                 {
                     if (attempt < MaxSyncRotationRetries - 1)
                     {
-                        // Transient lock. Use SpinWait to pause without dropping thread context.
-                        SpinWait.SpinUntil(() => false, SyncRotationRetryDelayMs);
+                        // Transient lock.
+                        Thread.Sleep(SyncRotationRetryDelayMs);
                     }
                     else
                     {
