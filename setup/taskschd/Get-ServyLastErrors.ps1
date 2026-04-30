5b4db5b22d569a9a0cd6d71d171d71dfd2f88bd1
aelassas <akram.elassas@gmail.com>
2026-04-30 18:49:20 +0100

    fix(notifications): ServyFailureNotification.ps1 - Toast Tag/Group both set to 'Servy' causes new toasts to silently replace earlier ones in Action Center fix(notifications): Get-ServyLastErrors.ps1 - DateTime.Parse uses current-culture, locale-dependent date parsing of LastProcessed

--- setup/taskschd/Get-ServyLastErrors.ps1
+++ setup/taskschd/Get-ServyLastErrors.ps1
@@ -39,7 +39,12 @@ function Get-ServyLastErrors {
 
   if ($null -ne $LastProcessed -and -not ($LastProcessed -is [datetime])) {
       try {
-          $LastProcessed = [DateTime]::Parse($LastProcessed)
+          $LastProcessed = [DateTime]::ParseExact(
+              $LastProcessed,
+              'o',
+              [System.Globalization.CultureInfo]::InvariantCulture,
+              [System.Globalization.DateTimeStyles]::RoundtripKind
+          )
       }
       catch {
           throw "Invalid datetime value for LastProcessed"
