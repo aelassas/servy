dc644cf2832634cdd77b4a0bbf97d2d2d9792b28
aelassas <akram.elassas@gmail.com>
2026-05-02 02:32:16 +0100

    fix(manager): App.xaml.cs - typo 'Dependencis' in DependenciesRefreshIntervalInMs XML doc

--- src/Servy.Manager/App.xaml.cs
+++ src/Servy.Manager/App.xaml.cs
@@ -150,7 +150,7 @@ namespace Servy.Manager
         }
 
         /// <summary>
-        /// Dependencis tab refresh interval in seconds.
+        /// Dependencies tab refresh interval in seconds.
         /// </summary>
         public int DependenciesRefreshIntervalInMs { get; private set; }
 
