62810ba13b7c895aaacd3f9501696364c6e09506
aelassas <akram.elassas@gmail.com>
2026-05-08 10:29:00 +0100

    test: fix ProcessLauncherIntegrationTests.cs

--- tests/Servy.Service.IntegrationTests/ProcessManagement/ProcessLauncherIntegrationTests.cs
+++ tests/Servy.Service.IntegrationTests/ProcessManagement/ProcessLauncherIntegrationTests.cs
@@ -50,7 +50,7 @@ namespace Servy.Service.IntegrationTests.ProcessManagement
         [InlineData(null)]
         public void Start_EmptyExecutable_ThrowsArgumentException(string exePath)
         {
-            var options = CreateOptions(exePath, string.Empty, false, 10_000));
+            var options = CreateOptions(exePath, string.Empty, false, 10_000);
             Assert.Throws<ArgumentException>(() => ProcessLauncher.Start(options, _realFactory, _logger));
         }
 
@@ -84,7 +84,7 @@ namespace Servy.Service.IntegrationTests.ProcessManagement
         public void Start_Synchronous_WaitsForExit_And_Heartbeats()
         {
             int heartbeats = 0;
-            var options = CreateOptions("powershell.exe", "-NoProfile -Command \"Write-Output 'OK'\"", fireAndForget: false, timeoutMs: 10_000));
+            var options = CreateOptions("powershell.exe", "-NoProfile -Command \"Write-Output 'OK'\"", fireAndForget: false, timeoutMs: 10_000);
             options.WaitChunkMs = 10;
             options.OnScmHeartbeat = new Action<int>((time) => Interlocked.Increment(ref heartbeats));
 
@@ -126,7 +126,7 @@ namespace Servy.Service.IntegrationTests.ProcessManagement
         public void Start_RedirectOutput_SamePath_WritesToSingleFileMultiplexed()
         {
             string logPath = CreateTempFilePath();
-            var options = CreateOptions("powershell.exe", "-NoProfile -Command \"Write-Output 'STDOUT_MSG'; [Console]::Error.WriteLine('STDERR_MSG')\"", false, 10_000));
+            var options = CreateOptions("powershell.exe", "-NoProfile -Command \"Write-Output 'STDOUT_MSG'; [Console]::Error.WriteLine('STDERR_MSG')\"", false, 10_000);
             options.EnableConsoleUI = false;
             options.RedirectToWriters = true;
             options.StdOutPath = logPath;
