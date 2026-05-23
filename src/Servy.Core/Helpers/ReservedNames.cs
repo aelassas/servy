3ba695c2fd1ce36ab65958aa439385eb833eb16f
aelassas <akram.elassas@gmail.com>
2026-05-23 14:21:43 +0100

    fix(core): use an immutable collection or reduce the accessibility of the non-private readonly field 'ReservedDeviceNames'

--- src/Servy.Core/Helpers/ReservedNames.cs
+++ src/Servy.Core/Helpers/ReservedNames.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace Servy.Core.Helpers
 {
@@ -9,16 +10,20 @@ namespace Servy.Core.Helpers
     public class ReservedNames
     {
         /// <summary>
-        /// A complete collection of legacy Windows reserved device names that cannot be used as filenames,
+        /// A complete immutable collection of legacy Windows reserved device names that cannot be used as filenames,
         /// including COM/LPT ports (0-9) and their Unicode superscript variants (¹, ², ³).
         /// </summary>
-        public static readonly HashSet<string> ReservedDeviceNames = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
-        {
+        /// <remarks>
+        /// Enforcing an <see cref="ImmutableHashSet{T}"/> guarantees structural integrity across the application 
+        /// lifecycle, preventing external consumers from mutating, clearing, or injecting values into the definition block.
+        /// </remarks>
+        public static readonly ImmutableHashSet<string> ReservedDeviceNames = ImmutableHashSet.Create(
+            StringComparer.OrdinalIgnoreCase,
             "CON", "PRN", "AUX", "NUL",
             "COM0", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
             "COM¹", "COM²", "COM³",
             "LPT0", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9",
             "LPT¹", "LPT²", "LPT³"
-        };
+        );
     }
 }
