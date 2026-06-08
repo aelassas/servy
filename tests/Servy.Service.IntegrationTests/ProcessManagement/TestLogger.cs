0129a541fb2d696b80e7fbe0ed6e54b2c3ba6cb6
aelassas <akram.elassas@gmail.com>
2026-06-08 23:20:18 +0100

    test(service): fix TestLogger.cs

--- tests/Servy.Service.IntegrationTests/ProcessManagement/TestLogger.cs
+++ tests/Servy.Service.IntegrationTests/ProcessManagement/TestLogger.cs
@@ -10,12 +10,12 @@ namespace Servy.Service.IntegrationTests.ProcessManagement
         public List<string> Warnings { get; } = new List<string>();
         public List<string> Errors { get; } = new List<string>();
 
-        public string? Prefix => string.Empty;
+        public string Prefix => string.Empty;
 
-        public void Info(string message, Exception? ex = null) => Infos.Add(message);
-        public void Warn(string message, Exception? ex = null) => Warnings.Add(message);
-        public void Error(string message, Exception? ex = null) => Errors.Add(message);
-        public void Debug(string message, Exception? ex = null) { }
+        public void Info(string message, Exception ex = null) => Infos.Add(message);
+        public void Warn(string message, Exception ex = null) => Warnings.Add(message);
+        public void Error(string message, Exception ex = null) => Errors.Add(message);
+        public void Debug(string message, Exception ex = null) { }
 
         public IServyLogger CreateScoped(string prefix) => throw new NotImplementedException();
         public void SetLogLevel(LogLevel level) { }
