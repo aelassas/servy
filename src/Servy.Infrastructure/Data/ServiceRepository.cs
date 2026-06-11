17e93b298c2700bc048dcb1ec9985352cf023dad
Akram El Assas <akram.elassas@gmail.com>
2026-06-11 15:32:35 +0100

    refactor(infra): update ServiceRepository.cs

--- src/Servy.Infrastructure/Data/ServiceRepository.cs
+++ src/Servy.Infrastructure/Data/ServiceRepository.cs
@@ -263,7 +263,7 @@ namespace Servy.Infrastructure.Data
                 dto = await _dapper.QuerySingleOrDefaultAsync<ServiceDto>(sql, new { Name = name }, cancellationToken: cancellationToken);
             }
 
-            if (decrypt && dto != null) SafeDecrypt(dto);
+            if (decrypt) SafeDecrypt(dto);
             return dto;
         }
 
