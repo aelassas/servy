93da8a1a3f399a4038c6cae0e0402bc6c0a44fc1
aelassas <1339626+aelassas@users.noreply.github.com>
2026-08-14 08:15:41 +0100

    test(manager): DependenciesViewModelTests.cs - #2484's name-overpromises-Logs half was never fixed, only the comment/assertion mismatch was (#5072)

--- tests/Servy.Manager.UnitTests/ViewModels/DependenciesViewModelTests.cs
+++ tests/Servy.Manager.UnitTests/ViewModels/DependenciesViewModelTests.cs
@@ -423,7 +423,7 @@ namespace Servy.Manager.UnitTests.ViewModels
         }
 
         [Fact]
-        public async Task LoadDependencyTreeAsync_ManagerThrowsException_LogsAndDisplaysErrorMessageBox()
+        public async Task LoadDependencyTreeAsync_ManagerThrowsException_DisplaysErrorMessageBox()
         {
             await Helper.RunOnSTA(async () =>
             {
