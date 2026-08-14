f2797cdfe6fa42bce1ec2529c3fe5af98733d238
aelassas <1339626+aelassas@users.noreply.github.com>
2026-08-14 09:18:03 +0100

    test(service): StreamWriterFactoryTests.cs - test.log still litters CWD, #2667's fix only added the using block

--- tests/Servy.Service.UnitTests/StreamWriters/StreamWriterFactoryTests.cs
+++ tests/Servy.Service.UnitTests/StreamWriters/StreamWriterFactoryTests.cs
@@ -1,6 +1,7 @@
 using Servy.Core.Enums;
 using Servy.Service.StreamWriters;
 using Servy.Testing;
+using System;
 using System.IO;
 using Xunit;
 
@@ -13,7 +14,7 @@ namespace Servy.Service.UnitTests.StreamWriters
         {
             // Arrange
             var factory = new StreamWriterFactory();
-            string path = "test.log";
+            string path = Path.Combine(Path.GetTempPath(), $"ServyTest_{Guid.NewGuid():N}.log");
             bool enableSizeRotation = true;
             long rotationSizeInBytes = 1024;
             bool enableDateRotation = false;
@@ -21,25 +22,35 @@ namespace Servy.Service.UnitTests.StreamWriters
             int maxRotations = 3;
             bool useLocalTime = true;
 
-            // Act & Assert
-            using (var result = factory.Create(
-                path,
-                enableSizeRotation,
-                rotationSizeInBytes,
-                enableDateRotation,
-                dateRotationType,
-                maxRotations,
-                useLocalTime))
+            try
             {
-                var adapter = Assert.IsType<RotatingStreamWriterAdapter>(result);
-                var inner = TestReflection.GetField<Core.IO.RotatingStreamWriter>(adapter, "_inner");
-                Assert.Equal(path, TestReflection.GetField<FileInfo>(inner, "_file").Name);
-                Assert.Equal(enableSizeRotation, TestReflection.GetField<bool>(inner, "_enableSizeRotation"));
-                Assert.Equal(rotationSizeInBytes, TestReflection.GetField<long>(inner, "_rotationSizeInBytes"));
-                Assert.Equal(enableDateRotation, TestReflection.GetField<bool>(inner, "_enableDateRotation"));
-                Assert.Equal(dateRotationType, TestReflection.GetField<DateRotationType>(inner, "_dateRotationType"));
-                Assert.Equal(maxRotations, TestReflection.GetField<int>(inner, "_maxRotations"));
-                Assert.Equal(useLocalTime, TestReflection.GetField<bool>(inner, "_useLocalTimeForRotation"));
+                // Act & Assert
+                using (var result = factory.Create(
+                    path,
+                    enableSizeRotation,
+                    rotationSizeInBytes,
+                    enableDateRotation,
+                    dateRotationType,
+                    maxRotations,
+                    useLocalTime))
+                {
+                    var adapter = Assert.IsType<RotatingStreamWriterAdapter>(result);
+                    var inner = TestReflection.GetField<Core.IO.RotatingStreamWriter>(adapter, "_inner");
+                    Assert.Equal(path, TestReflection.GetField<FileInfo>(inner, "_file").FullName);
+                    Assert.Equal(enableSizeRotation, TestReflection.GetField<bool>(inner, "_enableSizeRotation"));
+                    Assert.Equal(rotationSizeInBytes, TestReflection.GetField<long>(inner, "_rotationSizeInBytes"));
+                    Assert.Equal(enableDateRotation, TestReflection.GetField<bool>(inner, "_enableDateRotation"));
+                    Assert.Equal(dateRotationType, TestReflection.GetField<DateRotationType>(inner, "_dateRotationType"));
+                    Assert.Equal(maxRotations, TestReflection.GetField<int>(inner, "_maxRotations"));
+                    Assert.Equal(useLocalTime, TestReflection.GetField<bool>(inner, "_useLocalTimeForRotation"));
+                }
+            }
+            finally
+            {
+                if (File.Exists(path))
+                {
+                    try { File.Delete(path); } catch { /* Ignore exceptions */ }
+                }
             }
         }
     }
