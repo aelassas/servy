dcab7feab7c1e9945f431f922271c7b72d38511a
aelassas <1339626+aelassas@users.noreply.github.com>
2026-08-29 23:09:31 +0100

    chore: Servy-Restore.ps1 - broken verb parallelism in the Execution Privileges note ('to interact with ... and managing') (#6223)

--- src/Servy.CLI/Servy-Restore.ps1
+++ src/Servy.CLI/Servy-Restore.ps1
@@ -67,7 +67,7 @@
     - Operating System: Windows 7 SP1, Windows Server 2008 R2, or later.
     - PowerShell Version: Windows PowerShell 2.0 or higher.
     - Servy Core Components: Servy CLI and Servy PowerShell module (Servy.psm1) must be installed in %ProgramFiles%\Servy or portable root.
-    - Execution Privileges: Administrator privileges are required to interact with Servy configurations and managing Windows services.
+    - Execution Privileges: Administrator privileges are required to interact with Servy configurations and manage Windows services.
 #>
 [CmdletBinding()]
 param(
