0fa48b9f20bd982777a6adff15a191a2a4e7dc6e
aelassas <1339626+aelassas@users.noreply.github.com>
2026-09-09 14:51:39 +0100

    chore: Servy.Core.csproj - missing EOF newline, the only one of 22 csproj, contradicting #4755's own recorded fixed state (#6758) ci(choco): save chocolateyinstall.ps1 as UTF-8 with BOM

--- setup/choco/servy/tools/chocolateyinstall.ps1
+++ setup/choco/servy/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-# chocolateyinstall.ps1 contains URLs and checksums for the latest release available on GitHub releases page.
+﻿# chocolateyinstall.ps1 contains URLs and checksums for the latest release available on GitHub releases page.
 # URLs and checksums are auto-updated on each new release on GitHub through choco.yml workflow.
 
 $ErrorActionPreference = 'Stop'
@@ -8,10 +8,10 @@ $installerType = 'exe'
 $checksumType  = 'sha256'
 $silentArgs    = '/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /CLOSEAPPLICATIONS /NOCANCEL'
 
-$url64         = 'https://github.com/aelassas/servy/releases/download/v10.0/servy-10.0-x64-installer.exe'
+$url64         = 'https://github.com/aelassas/servy/releases/download/v9.9/servy-9.9-x64-installer.exe'
 $checksum64    = '034AC6823BDE7083A0F3D92D9A2AF61456A1F3380EF8C73E9AEE5C4F302999F8'
 
-$urlArm64      = 'https://github.com/aelassas/servy/releases/download/v10.0/servy-10.0-arm64-installer.exe'
+$urlArm64      = 'https://github.com/aelassas/servy/releases/download/v9.9/servy-9.9-arm64-installer.exe'
 $checksumArm64 = '35C28288F68BDAB2FB909F17425495B84AF231A812FCB3FC8BC48AD401615A4B'
 
 # Detect OS architecture dynamically
